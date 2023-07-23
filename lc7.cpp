b1a71b69f57c21f825b8d8dcec12c0eb5dd6a504
Lakshay Agarwal <633lakshay@gmail.com>
2023-07-23 21:02:10 +0530

    Add files via upload

---
+++ lc7.cpp
@@ -0,0 +1,18 @@
+//reverse integer
+
+class Solution {
+public:
+    int reverse(int x) {
+        int ans = 0;
+        while (x!=0){
+            int a = x%10;
+            
+            if((ans>(INT_MAX/10))||(ans<(INT_MIN/10))){
+                return 0;            
+            }
+            ans = (ans*10)+a; 
+            x=x/10;
+        }
+        return ans;
+    }
+};
