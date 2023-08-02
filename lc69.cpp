64ed87dee70768f2d4b540069f9f46825b6c5087
Lakshay Agarwal <633lakshay@gmail.com>
2023-08-02 14:45:19 +0530

    Update lc69.cpp

--- lc69.cpp
+++ lc69.cpp
@@ -5,7 +5,23 @@
 using namespace std;
 
 int sqrt(int n){
-    
+    int mid=s+((e-s)/2); //modified (s+e)/2 so that max of int isn't reached
+    int ans = -1;
+    while (s<=e){
+        long long int sq = mid*mid;
+        if(sq == n){
+            return mid;
+        }
+        else if (sq<n){
+            ans = mid;
+            s=mid+1;
+        }
+        else{
+            e=mid-1;
+        }
+        mid=s+((e-s)/2);
+    }
+    return ans;
 }
 
 int main()
