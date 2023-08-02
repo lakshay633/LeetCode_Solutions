f6af41bb4735f7edcfbac52feb7a14be379cda38
Lakshay Agarwal <633lakshay@gmail.com>
2023-08-02 14:40:37 +0530

    Add files via upload

---
+++ lc69.cpp
@@ -0,0 +1,17 @@
+// Sqrt(x)
+// https://leetcode.com/problems/sqrtx/
+
+#include <iostream>
+using namespace std;
+
+int sqrt(int n){
+    
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    cout <<sqrt(n);
+    return 0;
+}
