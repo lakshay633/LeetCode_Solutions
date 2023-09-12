f96bca0712b34754df1cd141802ce2d89ca7d5f2
Lakshay Agarwal <633lakshay@gmail.com>
2023-09-12 08:28:52 +0530

    Add files via upload

---
+++ lc1752.cpp
@@ -0,0 +1,17 @@
+//Check if Array Is Sorted and Rotated
+
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int count = 0;
+        for (int i=1;i<nums.size();i++){
+            if(nums[i-1]>nums[i]){
+                count++;
+            }
+        }
+        if(nums[(nums.size()-1)]>nums[0]){
+            count++;
+        }
+        return count<=1;
+    }
+};
