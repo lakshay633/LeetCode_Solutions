e3aa26fc726798175b9dfab91e8021484bca7058
Lakshay Agarwal <633lakshay@gmail.com>
2023-09-15 18:31:42 +0530

    Add files via upload

---
+++ lc344.cpp
@@ -0,0 +1,15 @@
+// reverse a string
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int b=0;
+        int e = s.size() - 1;
+        while(b<e){
+            swap(s[b],s[e]);
+            b++;
+            e--;
+        }
+        
+    }
+};
