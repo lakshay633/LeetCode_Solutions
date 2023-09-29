6aff757070df9d4d78cc08315327202a1ec486a2
Lakshay Agarwal <633lakshay@gmail.com>
2023-09-29 14:06:31 +0530

    Add files via upload

---
+++ lc1910.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+string removeOccurrences(string s, string part) {
+    while(s.length()!=0 && s.find(part) < s.length()){
+        s.erase(s.find(part),part.length());
+    }
+    return s;
+}
+int main()
+{
+    string s = "daabcbaabcbc";
+    string part = "abc";
+    string res = removeOccurrences(s,part);
+    cout <<res;
+    return 0;
+}
+
