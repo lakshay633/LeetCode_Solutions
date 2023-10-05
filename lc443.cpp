8dce8c447a1c3027e4dc35a8995125544e2939e0
Lakshay Agarwal <633lakshay@gmail.com>
2023-10-05 21:34:53 +0530

    Add files via upload

---
+++ lc443.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+int compress(vector<char> &chars)
+{
+    int i = 0;
+    int ansIndex = 0;
+    int n = chars.size();
+    while (i < n)
+    {
+        int j = i + 1;
+        while (j < n && chars[i] == chars[j])
+        {
+            j++;
+        }
+        chars[ansIndex++] = chars[i];
+        int count = j - i;
+        if (count > 1)
+        {
+            string cnt = to_string(count);
+            for (char ch : cnt)
+            {
+                chars[ansIndex++] = ch;
+            }
+        }
+        i = j;
+    }
+    for (char x : chars)
+        cout << x << " ";
+    cout<<endl;
+    return ansIndex;
+}
+
+int main()
+{
+    vector<char> chars {'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'};
+    int res = compress(chars);
+    cout <<res;
+    return 0;
+}
