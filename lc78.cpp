0cb3f8c202b56cdd05bfdb8a9898a121524719b6
Lakshay Agarwal <633lakshay@gmail.com>
2024-02-20 08:31:09 +0530

    Add files via upload

---
+++ lc78.cpp
@@ -0,0 +1,23 @@
+class Solution {
+private:
+    void solve(vector<int>& nums,vector<int> output,int index,vector<vector<int>>& ans){
+        if(index>=nums.size()){
+            ans.push_back(output);
+            return;
+        }
+        //exclude
+        solve(nums,output,index+1,ans);
+        //include
+        // int element=nums[i];
+        output.push_back(nums[index]);
+        solve(nums,output,index+1,ans);
+    }
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> output;
+        int index=0;
+        solve(nums,output,index,ans);
+        return ans;
+    }
+};
