4735ab18b5f38092aeff46112cb3c79f4088b48f
Lakshay Agarwal <633lakshay@gmail.com>
2024-04-12 08:54:55 +0530

    Add files via upload

---
+++ lc232.cpp
@@ -0,0 +1,37 @@
+class MyQueue {
+public:
+    stack<int> s1, s2;
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        s1.push(x);
+    }
+    
+    int pop() {
+        if (s2.empty()) {
+            while (!s1.empty()) {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+        int topElement = s2.top();
+        s2.pop();
+        return topElement;
+    }
+    
+    int peek() {
+        if (s2.empty()) {
+            while (!s1.empty()) {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+        return s2.top();
+    }
+    
+    bool empty() {
+        return s1.empty() && s2.empty();
+    }
+};
