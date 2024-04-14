86c5916dabfb9ff56bc8b9b58725053e2ff5af0c
Lakshay Agarwal <633lakshay@gmail.com>
2024-04-14 14:06:58 +0530

    Update lc70.cpp

--- lc70.cpp
+++ lc70.cpp
@@ -1,22 +1,44 @@
+// class Solution {
+// public:
+//     int climbStairs(int n) {
+//         if (n < 0)
+//             return 0;
+//         if (n == 0 || n == 1)
+//             return 1;
+
+//         vector<int> memo(n + 1, 0);
+//         memo[0] = memo[1] = 1;
+
+//         return solve(n, memo);
+//     }
+
+//     int solve(int n, vector<int>& memo) {
+//         if (memo[n] != 0)
+//             return memo[n];
+
+//         memo[n] = solve(n - 1, memo) + solve(n - 2, memo);
+//         return memo[n];
+//     }
+// };
+#include <vector>
+
 class Solution {
 public:
     int climbStairs(int n) {
         if (n < 0)
             return 0;
-        if (n == 0 || n == 1)
-            return 1;
+        if (n <= 2)
+            return n;
 
-        vector<int> memo(n + 1, 0);
-        memo[0] = memo[1] = 1;
+        std::vector<int> memo(n + 1, 0);
+        memo[0] = 1;
+        memo[1] = 1;
+        memo[2] = 2;
 
-        return solve(n, memo);
-    }
+        for (int i = 3; i <= n; i++) {
+            memo[i] = memo[i - 1] + memo[i - 2];
+        }
 
-    int solve(int n, vector<int>& memo) {
-        if (memo[n] != 0)
-            return memo[n];
-
-        memo[n] = solve(n - 1, memo) + solve(n - 2, memo);
         return memo[n];
     }
 };
