87f91520445e14daf176af54799c62beecd327a4
Lakshay Agarwal <633lakshay@gmail.com>
2024-05-18 14:13:51 +0530

    Add files via upload

---
+++ lc62.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int t[101][101];
+    int solve(int i, int j, int m , int n){
+        if(i>=m || j >=n){
+            return 0;
+        }
+        if( i==m-1 && j==n-1){
+            return 1;
+        }
+        if(t[i][j] != -1 ){
+            return t[i][j];
+        }
+        int right = solve(i,j+1,m,n);
+        int down = solve(i+1,j,m,n);
+        return t[i][j] = right + down;
+    }
+    int uniquePaths(int m, int n) {
+        memset(t,-1,sizeof(t));
+        return solve(0,0,m,n);
+    }
+};
